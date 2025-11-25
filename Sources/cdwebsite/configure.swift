9a786ade3a8ab064c57ef639d4866390f31b67e8
David Vales <davidvales@valescorp.org>
2025-11-24 20:32:08 -0600

    cambios importantes
    cambios

--- Sources/cdwebsite/configure.swift
+++ Sources/cdwebsite/configure.swift
@@ -7,8 +7,8 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
     // para aceptar todas las conexiones
-    //app.http.server.configuration.hostname = "::"
-    //app.http.server.configuration.port = 8080
+    app.http.server.configuration.hostname = "::"
+    app.http.server.configuration.port = 8080
     
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
