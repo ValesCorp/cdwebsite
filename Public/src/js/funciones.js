9831827f5fc182dd233e6162fff926b9c4ef8b44
David Vales <davidvales@valescorp.org>
2025-11-30 22:20:36 -0600

    cambios
    cambios

--- Public/src/js/funciones.js
+++ Public/src/js/funciones.js
@@ -3,30 +3,13 @@
 // Vales Corp
 //
 // Para funciones de página, van específicamente funciones, disparadores, etc.
-document.addEventListener("DOMContentLoaded", function () {
 
-    const toggle = document.getElementById("listenToggle");
-    const menu = document.getElementById("fsMenu");
-    const overlay = document.getElementById("fsOverlay");
-    const closeBtn = document.getElementById("closeFsMenu");
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
-    function openMenu() {
-        menu.classList.add("show");
-        overlay.style.display = "block";
-    }
-
-    function closeMenu() {
-        menu.classList.remove("show");
-        overlay.style.display = "none";
-    }
-
-    toggle.addEventListener("click", function(e) {
-        e.preventDefault();
-        e.stopPropagation(); // evita conflicto con Semantic
-        openMenu();
-    });
-
-    overlay.addEventListener("click", closeMenu);
-    closeBtn.addEventListener("click", closeMenu);
-});
-
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
